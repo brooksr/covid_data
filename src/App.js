996034667fa78dc21100bc195322b5e4c328b6cc
brooksroche <broche88@gmail.com>
2020-06-27 16:06:24 -0700

    simple CA positive graph to start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+const axios = require("axios");
 
 function App() {
+  let [data, setData] = useState({}, null);
+
+  useEffect(() => {
+    axios.get("https://covidtracking.com/api/states/daily").then(function (response) {
+      // handle success
+      setData(response)
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>COVID 19 Data</h1>
+      <div className="flex">
+        <div className="graph">
+        <LineChart width={400} height={400} data={!data.data ? [] : data.data.filter(i => i.state == "CA").reverse()}>
+          <Line type="monotone" dataKey="positive" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis dataKey="date" />
+          <YAxis dataKey="positive" />
+          <Tooltip></Tooltip>
+        </LineChart>
+        </div>
+        <textarea value={JSON.stringify(data, null, "\t")} className="raw-data" />
+      </div>
     </div>
   );
 }
